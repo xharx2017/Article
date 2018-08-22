fa289530d324ea02e8391d4c9b7b6aab97831d87
xharx2017 <yunchang365@outlook.com>
2018-08-22 09:50:57 +0800

    增加统计应支付给工人的装卸费和分拣费

--- 20180821装卸费分拣费报表-改用全部查询.sql
+++ 20180821装卸费分拣费报表-改用全部查询.sql
@@ -1,13 +1,15 @@
 /*
   金蝶K/3Cloud的直接SQL账表，只支持select语句，即纯查询。其他诸如Create，Insert，Delete，Update，With table As等等都不支持。
   这坑爹的玩意，让一张简单的报表搞的无比复杂。
+  2018-08-21:统计应收货主的装卸费和分拣费；
+  2018-08-22:按王静要求，增加统计应支付给工人的装卸费和分拣费；
 */
 select * from
 (
 select c.FNumber,cl.FName,b.billNo,to_char(b.ywdate,'yyyy-MM-dd') ywdate,
        m.FNumber GoodNumber,ml.FName GoodName,ml.FSPECIFICATION,m.FOLDNUMBER,al.FDataValue,ul.FName Unit,
-       b.qty,b.zxfprice,b.zxf,b.fjfprice,b.fjf,b.expl,
-       case b.bill when 1 then '其他入库' else '其他出库' end billt
+       b.qty,b.zxfprice,b.zxf,b.fjfprice,b.fjf,b.F_UX_WORKERPRICE_D,b.workamount,b.F_UX_FJOUTPRICE,b.F_UX_FJWORKERAMOUNT,
+       b.expl,case b.bill when 1 then '其他入库' else '其他出库' end billt
 from
 (
 --查询入库明细数据
@@ -17,6 +19,8 @@ from
         case when m.Fstockdirect='GENERAL' then d.f_ux_inamount_d else -d.f_ux_inamount_d end zxf,
         d.f_ux_fjinprice fjfPrice,
         case when m.Fstockdirect='GENERAL' then d.f_ux_fjowneramount else -d.f_ux_fjowneramount end fjf,
+        d.F_UX_WORKERPRICE_D,case when m.Fstockdirect='GENERAL' then d.F_UX_WORKAMOUNT_D else -d.F_UX_WORKAMOUNT_D end workamount, 
+        d.F_UX_FJOUTPRICE,case when m.Fstockdirect='GENERAL' then d.F_UX_FJWORKERAMOUNT else -d.F_UX_FJWORKERAMOUNT end F_UX_FJWORKERAMOUNT, 
         d.fnote expl,m.fdate ywdate,1 bill
   from t_stk_miscellaneous m
    left join t_stk_miscellaneousentry d on m.fid=d.fid
@@ -30,6 +34,8 @@ from
         d.f_Ux_Unloadcosttype_d,
         d.F_UX_OUTPRICE_D,case when m.Fstockdirect='GENERAL' then d.F_UX_OUTAMOUNT_D else -F_UX_OUTAMOUNT_D end,
         d.F_UX_FJINPRICE,case when m.Fstockdirect='GENERAL' then d.F_UX_FJOWNERAMOUNT else -d.F_UX_FJOWNERAMOUNT end,
+        d.F_UX_WORKERPRICE_D,case when m.Fstockdirect='GENERAL' then d.F_UX_WORKAMOUNT_D else -d.F_UX_WORKAMOUNT_D end,
+        d.F_UX_FJOUTPRICE,case when m.Fstockdirect='GENERAL' then d.F_UX_FJWORKERAMOUNT else -d.F_UX_FJWORKERAMOUNT end,
         d.fnote,m.fdate,2
   from t_STK_MisDelivery m
    left join t_STK_MisDeliveryentry d on m.fid=d.fid
@@ -48,7 +54,8 @@ from
 Union All
 select c.FNumber,cl.FName||'-合计',N'','',
        N'',N'',N'',N'',N'',N'',
-        sum(b.qty),0,sum(b.zxf),0,sum(b.fjf),N'',''
+       sum(b.qty),0,sum(b.zxf),0,sum(b.fjf),0,sum(b.workamount),0,sum(b.F_UX_FJWORKERAMOUNT),
+       N'',''
 from 
 (
 --查询入库明细数据
@@ -58,6 +65,8 @@ from
         case when m.Fstockdirect='GENERAL' then d.f_ux_inamount_d else -d.f_ux_inamount_d end zxf,
         d.f_ux_fjinprice fjfPrice,
         case when m.Fstockdirect='GENERAL' then d.f_ux_fjowneramount else -d.f_ux_fjowneramount end fjf,
+        d.F_UX_WORKERPRICE_D,case when m.Fstockdirect='GENERAL' then d.F_UX_WORKAMOUNT_D else -d.F_UX_WORKAMOUNT_D end workamount, 
+        d.F_UX_FJOUTPRICE,case when m.Fstockdirect='GENERAL' then d.F_UX_FJWORKERAMOUNT else -d.F_UX_FJWORKERAMOUNT end F_UX_FJWORKERAMOUNT, 
         d.fnote expl,m.fdate ywdate,1 bill
   from t_stk_miscellaneous m
    left join t_stk_miscellaneousentry d on m.fid=d.fid
@@ -71,6 +80,8 @@ from
         d.f_Ux_Unloadcosttype_d,
         d.F_UX_OUTPRICE_D,case when m.Fstockdirect='GENERAL' then d.F_UX_OUTAMOUNT_D else -F_UX_OUTAMOUNT_D end,
         d.F_UX_FJINPRICE,case when m.Fstockdirect='GENERAL' then d.F_UX_FJOWNERAMOUNT else -d.F_UX_FJOWNERAMOUNT end,
+        d.F_UX_WORKERPRICE_D,case when m.Fstockdirect='GENERAL' then d.F_UX_WORKAMOUNT_D else -d.F_UX_WORKAMOUNT_D end,
+        d.F_UX_FJOUTPRICE,case when m.Fstockdirect='GENERAL' then d.F_UX_FJWORKERAMOUNT else -d.F_UX_FJWORKERAMOUNT end,
         d.fnote,m.fdate,2
   from t_STK_MisDelivery m
    left join t_STK_MisDeliveryentry d on m.fid=d.fid
@@ -85,7 +96,8 @@ from
 Union all
 select N'',N'总计',N'','',
        N'',N'',N'',N'',N'',N'',
-       sum(b.qty),0,sum(b.zxf),0,sum(b.fjf),N'',''
+       sum(b.qty),0,sum(b.zxf),0,sum(b.fjf),0,sum(b.workamount),0,sum(b.F_UX_FJWORKERAMOUNT),
+       N'',''
 from
 (
 --查询入库明细数据
@@ -95,6 +107,8 @@ from
         case when m.Fstockdirect='GENERAL' then d.f_ux_inamount_d else -d.f_ux_inamount_d end zxf,
         d.f_ux_fjinprice fjfPrice,
         case when m.Fstockdirect='GENERAL' then d.f_ux_fjowneramount else -d.f_ux_fjowneramount end fjf,
+        d.F_UX_WORKERPRICE_D,case when m.Fstockdirect='GENERAL' then d.F_UX_WORKAMOUNT_D else -d.F_UX_WORKAMOUNT_D end workamount, 
+        d.F_UX_FJOUTPRICE,case when m.Fstockdirect='GENERAL' then d.F_UX_FJWORKERAMOUNT else -d.F_UX_FJWORKERAMOUNT end F_UX_FJWORKERAMOUNT, 
         d.fnote expl,m.fdate ywdate,1 bill
   from t_stk_miscellaneous m
    left join t_stk_miscellaneousentry d on m.fid=d.fid
@@ -108,6 +122,8 @@ from
         d.f_Ux_Unloadcosttype_d,
         d.F_UX_OUTPRICE_D,case when m.Fstockdirect='GENERAL' then d.F_UX_OUTAMOUNT_D else -F_UX_OUTAMOUNT_D end,
         d.F_UX_FJINPRICE,case when m.Fstockdirect='GENERAL' then d.F_UX_FJOWNERAMOUNT else -d.F_UX_FJOWNERAMOUNT end,
+        d.F_UX_WORKERPRICE_D,case when m.Fstockdirect='GENERAL' then d.F_UX_WORKAMOUNT_D else -d.F_UX_WORKAMOUNT_D end,
+        d.F_UX_FJOUTPRICE,case when m.Fstockdirect='GENERAL' then d.F_UX_FJWORKERAMOUNT else -d.F_UX_FJWORKERAMOUNT end,
         d.fnote,m.fdate,2
   from t_STK_MisDelivery m
    left join t_STK_MisDeliveryentry d on m.fid=d.fid
@@ -118,50 +134,6 @@ from
 ) b
 ) c
 order by FNumber,ywdate,billno,goodname
-
-
-
---修改后，将单据上的单位统一换算为基本库存单位
-select c.FNumber,cl.FName,b.billNo,to_char(b.ywdate,'yyyy-MM-dd') ywdate,
-       m.FNumber GoodNumber,ml.FName GoodName,ml.FSPECIFICATION,m.FOLDNUMBER,al.FDataValue,ul.FName Unit,
-       b.qty,b.zxfprice,b.zxf,b.fjfprice,b.fjf,b.expl,
-       case b.bill when 1 then '其他入库' else '其他出库' end billt
- From 
- (--查询入库明细数据
-  select m.Fownerid custID,m.FBillNo billNo,d.Fmaterialid goodID,d.Fbaseunitid unitID,
-        case when m.Fstockdirect='GENERAL' then d.FbaseQty else -d.FbaseQty end qty,
-        d.f_Ux_Unloadcosttype_d costID,d.f_ux_inprice_d zxfPrice,
-        case when m.Fstockdirect='GENERAL' then d.f_ux_inamount_d else -d.f_ux_inamount_d end zxf,
-        d.f_ux_fjinprice fjfPrice,
-        case when m.Fstockdirect='GENERAL' then d.f_ux_fjowneramount else -d.f_ux_fjowneramount end fjf,
-        d.fnote expl,m.fdate ywdate,1 bill
-  from t_stk_miscellaneous m
-   left join t_stk_miscellaneousentry d on m.fid=d.fid
-   left join t_bd_customer c on m.Fownerid=c.FCustID
-  where m.fbilltypeid='5a29071d94fa9f' and m.Fownertypeid='BD_Customer' and d.f_Ux_Unloadcosttype_d !=' '
-    and m.FDate>=to_date('#startDate#','YYYY-MM-dd') and m.FDate<=to_date('#endDate#','yyyy-MM-dd')
-    and c.FNumber>='#startCustNo#' and c.FNumber<='#endCustNo#' 
-  Union ALL
- --查询出库明细数据
-  select m.Fownerid,m.FBillNo,d.Fmaterialid,d.Fbaseunitid,case when m.Fstockdirect='GENERAL' then d.FbaseQty else -d.FbaseQty end,
-        d.f_Ux_Unloadcosttype_d,
-        d.F_UX_OUTPRICE_D,case when m.Fstockdirect='GENERAL' then d.F_UX_OUTAMOUNT_D else -d.F_UX_OUTAMOUNT_D end,
-        d.F_UX_FJINPRICE,case when m.Fstockdirect='GENERAL' then d.F_UX_FJOWNERAMOUNT else -d.F_UX_FJOWNERAMOUNT end,
-        d.fnote,m.fdate,2
-  from t_STK_MisDelivery m
-   left join t_STK_MisDeliveryentry d on m.fid=d.fid
-   left join t_bd_customer c on m.Fownerid=c.FCustID
-  where m.fbilltypeid='5a2b78fed127a0' and m.Fownertypeid='BD_Customer' and d.f_Ux_Unloadcosttype_d !=' '
-    and m.FDate>=to_date('#startDate#','YYYY-MM-dd') and m.FDate<=to_date('#endDate#','yyyy-MM-dd')
-    and c.FNumber>='#startCustNo#' and c.FNumber<='#endCustNo#') b
-   left join t_bd_customer c               on b.custid=c.Fcustid
-   left join t_bd_customer_l cl            on c.fcustid=cl.fcustid         and cl.flocaleid=2052
-   left join t_bd_material m               on b.goodid=m.fmaterialid
-   left join t_bd_material_l ml            on m.fmaterialid=ml.fmaterialid and ml.flocaleid=2052  
-   left join t_bas_assistantdataentry_l al on b.costid=al.fentryid         and al.flocaleid=2052
-   left join t_bd_unit u                   on b.UnitID=u.Funitid
-   left join t_bd_unit_l ul                on u.funitid=ul.funitid         and ul.flocaleid=2052
-  order by c.FNumber,ywdate,b.billNo,m.FNumber
 
 --附上用With语法的报表
 with 
